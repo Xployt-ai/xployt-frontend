67109af4123b634da4e22580a610fb21a3117df2
safran zahim <safran1600014@gmail.com>
2025-07-13 16:46:13 +0530

    feat: add Successful page component and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Successful from "./pages/Successful";
+import Successful from "./pages/Successful.tsx";
 
 
 function App() {
