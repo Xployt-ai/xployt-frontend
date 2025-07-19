2657f504d0cb14da95ccee59e0b0f0a47c539802
safran zahim <safran1600014@gmail.com>
2025-07-19 11:08:14 +0530

    fix: refactor Input component layout and remove focus styles; add BillCard component with payment details

---
+++ src/features/BillCard.tsx
@@ -0,0 +1,30 @@
+import { Card } from "@/components/ui/Card";
+import {TypographyH1 , TypographyP ,TypographyH4, TypographySmall} from '@/components/ui/typography'
+import card from "@/assets/card.svg"; 
+import { Button } from "@/components/ui/Button";
+
+const BillCard = () => {
+  return (
+    <Card className="flex flex-row max-w-full justify-between">
+      <div >
+      <TypographyH1>Standard Plan</TypographyH1>
+      <TypographyH4 className="text-green-500">$5/month</TypographyH4>
+      <TypographySmall>Add Bill Payment Method /// change</TypographySmall>
+      <div className="mt-4 flex ">
+        <img src={card} alt="Card Image" className="pr-4" />
+
+          <TypographySmall >Visa ending in 4242</TypographySmall>
+        </div>
+      </div>
+      <div className=" flex flex-col items-center justify-center ">
+        <Button className="mb-4">Update Payment Method</Button>
+        <Button variant="outline" >Update Payment Method</Button>
+      </div>
+
+    </Card>
+
+
+  );
+};
+
+export default BillCard;
--- src/pages/Bill.tsx
+++ src/pages/Bill.tsx
@@ -1,5 +1,6 @@
 import UNavBar from "@/components/UNavbar";
 import { TypographyH1, TypographyP } from "@/components/ui/typography";
+import BillCard from "@/features/BillCard";
 
 const Bill = () => {
   return (
@@ -7,14 +8,17 @@ const Bill = () => {
       <UNavBar />
       <div className='bg-black min-h-screen'>    
         <div className="w-full max-w-4xl mx-auto py-6 px-0 bg-transparent min-h-screen">
-          <div>
+          <div className="flex flex-col items-start justify-start ">
             <TypographyH1>
               Billing
             </TypographyH1>
-            <TypographyP>
+            <TypographyP className="text-slate-400">
                 Manage your Xployt.ai subscription and payment history.
             </TypographyP>
-        </div>    
+        </div>  
+          
+              <BillCard />
+
     </div>
     </div>
     </>
