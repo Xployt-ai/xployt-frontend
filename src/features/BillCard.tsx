a32bfee30c5c335db216c757dccd54731ee846e0
safran zahim <safran1600014@gmail.com>
2025-07-19 15:46:02 +0530

    fix: update Cancel Subscription button styling for consistency

--- src/features/BillCard.tsx
+++ src/features/BillCard.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/Button";
 
 const BillCard = () => {
   return (
-    <Card className="flex flex-row max-w-full justify-between">
+    <Card className="flex flex-row max-w-full justify-between ">
       <div >
       <TypographyH1>Standard Plan</TypographyH1>
       <TypographyH4 className="text-green-500">$5/month</TypographyH4>
@@ -18,7 +18,7 @@ const BillCard = () => {
       </div>
       <div className=" flex flex-col items-center justify-center ">
         <Button className="mb-4 ">Update Payment Method</Button>
-        <Button variant="outline" className="text-red-700" >Update Payment Method</Button>
+        <Button variant="outline" className="text-red-700 w-full " >Cancel Subscription</Button>
       </div>
 
     </Card>
