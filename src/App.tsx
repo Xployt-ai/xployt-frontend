f1488cb4e4deef3b0e3454da95ec84312c41d526
madushika <madushikanm2025@gmail.com>
2025-07-21 11:59:04 +0530

    fix: update routes in the pages

--- src/App.tsx
+++ src/App.tsx
@@ -6,17 +6,13 @@ import Documentation from "./pages/Documentation";
 import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import NewScan from "./pages/NewScan";
-import IssueDetail from "./pages/IssueDetail";
-import RepoImport from "./pages/RepoImport";
-
-
 import Successful from "./pages/Successful";
-
-
 import Dashboard from "./pages/Dashboard";
 import Bill from "./pages/Bill";
 import ScanningPage from "./pages/ScanningPage";
 import UNavBar from "@/components/UNavbar.tsx";
+import IssueDetail from "./pages/IssueDetail";
+import RepoImport from "./pages/RepoImport";
 
 
 function App() {
@@ -26,21 +22,23 @@ function App() {
         <UNavBar />
         <Routes>
 
+          <Route path="/" element={<Home />} />
+    
+          <Route path="/about" element={<About />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/docs" element={<Documentation />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newscan" element={<NewScan />} />
+          <Route path="/Dashboard" element={<Dashboard />} />
+          <Route path="/successful" element={<Successful />} />
+           <Route path="/repoimport" element={<RepoImport />} />
+          <Route path="/bill" element={<Bill />} />
+          <Route path="/scanning" element={<ScanningPage />} />
+            <Route path="/issuedetail" element={<IssueDetail />} />
 
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/pricing" element={<Pricing />} />
-            <Route path="/docs" element={<Documentation />} />
-            <Route path="/contact" element={<Contact />} />
-            <Route path="/new-scan" element={<NewScan />} />
-            <Route path="/Dashboard" element={<Dashboard />} />
-            <Route path="/successful" element={<Successful />} />
-             <Route path="/repo-import" element={<RepoImport />} />
-                 <Route path="/issuedetail" element={<IssueDetail />} />
-                  <Route path="/scanning" element={<ScanningPage />} />
-                <Route path="/bill" element={<Bill />} />
-          <Route path="/securitydashboard" element={<SecurityDashboardPage />} />
 
+
+        </Routes>
       </main>
     </BrowserRouter>
   );
